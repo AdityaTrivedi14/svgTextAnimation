386b6ae4ed0dbc3aed7c4989d36b758291063610
Aditya Trivedi <adityakuldeeptrivedi@gmail.com>
2020-12-04 03:01:32 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+//Using this we can calculate the stroke dashoffset that is the perimeter of each letter
+
 const logo = document.querySelectorAll('#text path');
 
 for(let i = 0; i < logo.length; i++){
